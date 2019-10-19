8971fe7f0a6a6565f46115231069add5a4c6a3f0
hhu <hhu@thoughtworks.com>
2019-10-20 01:02:15 +0800

    add mappers tests

--- jersey-with-mybatis/src/test/java/study/huhao/demo/infrastructure/persistence/MapperTest.java
+++ jersey-with-mybatis/src/test/java/study/huhao/demo/infrastructure/persistence/MapperTest.java
@@ -1,29 +1,21 @@
-package study.huhao.demo.adapters.restapi.resources;
+package study.huhao.demo.infrastructure.persistence;
 
-import io.restassured.RestAssured;
 import org.flywaydb.test.FlywayTestExecutionListener;
 import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class, FlywayTestExecutionListener.class})
-public abstract class ResourceTest {
-
-    @LocalServerPort
-    private int port;
+public class MapperTest {
 
     @BeforeEach
     @FlywayTest
     void init() {
-        RestAssured.port = port;
+
     }
 }
